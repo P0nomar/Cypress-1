dd7a038b18fe35a19fb0203c199d64c8bea3abff
P0nomar <trolltarasovich@gmail.com>
2021-08-10 11:27:33 +0300

    Solution1

--- cypress_test_project/cypress/integration/advanced.spec.js
+++ cypress_test_project/cypress/integration/advanced.spec.js
@@ -1,25 +1,19 @@
 /// <reference types="cypress" />
+let user;
+describe('Succesfull registration',() => {
 
-describe('',() => {
-    let user;
-
-    before('',() => {
+    before('Visit website',() => {
 cy.visit('http://localhost:1667/#/register');
-});
-describe('Successful registration', () => {
-    before(function () {
       cy.task("freshUser").then((object) => {
         user = object;
       });
     });
-});
 
 it('Sign up with faker', () => {
     cy.get(':nth-child(1) > .form-control').type(user.username);
     cy.get(':nth-child(2) > .form-control').type(user.email);
     cy.get(':nth-child(3) > .form-control').type(user.password);
     cy.get('.btn').click();
-    cy.get('.swal-modal').should('contain','Your registration was succesfull');
     cy.get(':nth-child(4) > .nav-link').should('contain',user.username);
 })
 })
